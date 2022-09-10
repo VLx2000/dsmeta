47deee347f5de4421894513dfd55acba316b4811
VLx2000 <victor.antunes.2000@gmail.com>
2022-09-10 01:07:08 -0300

    Other components

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -3,10 +3,8 @@ import './styles.css';
 
 function NotificationButton() {
   return (
-    <div className="dsmeta-red-btn-container">
-      <div className="dsmeta-red-btn">
-        <img src={icon} alt="Notificar" />
-      </div>
+    <div className="dsmeta-red-btn">
+      <img src={icon} alt="Notificar" />
     </div>
   )
 }
